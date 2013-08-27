b79521b55215ffd3fddf278296b3a063a629e0cf
Allison <asheren@neighborsations.com>
2013-08-26 21:13:32 -0400

    sorted index page by cook on date

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,4 @@
 class Recipe < ActiveRecord::Base
 	acts_as_taggable_on :tags
-
-	# belongs_to :recipe_link,  <%= link_to @recipe.url, @recipe.url %>
-	#another idea: validates? although link isn't required for saving which is what 
-	# validates is used for
+	
 end
