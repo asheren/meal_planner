924fe3a5999eb0b429c8626709eebdb23f56003b
Allison <asheren@neighborsations.com>
2013-09-11 12:50:20 -0400

    changed rspec tests after peer feedback to make better tests

--- spec/features/recipes_view_spec.rb
+++ spec/features/recipes_view_spec.rb
@@ -2,33 +2,41 @@ require 'spec_helper'
 require 'capybara/rails'
 require 'capybara/rspec'
 
-describe "the recipe view", :type => :feature do 
+describe "working with recipes", :type => :feature do 
+
+	context "when viewing a specific recipe" do 
+		let(:recipe) {Recipe.new} #is this correct?
 	
-	describe 'recipes' do 
 	
-	before :each do
-		recipe.make(:name => 'example')
-		visit recipe_path(recipe)
+	  it "shows the recipe" do
+		  visit recipe_path(recipe)
+		  expect(page).to have_content 'Put the recipe content here'
+	  end
+
+		it "has a link to add a new recipe" do 
+			visit recipe_path(recipe)
+
+		  expect(page).to have_link('New', href: new_recipe_path(recipe))
+	  end
 	end
 
-	it 'shows the recipes' do 
-		recipe.each do |recipe|
-			expect(page).to have_content(recipes)
+	context "when creating a new recipe" do
+		let(:baked_ziti) { Recipe.new name: 'Baked Ziti', link: 'http://www.food52.com' }
+
+		def add_new_recipe(recipe)
+			within('#recipe') do
+			  fill_in 'name', :with => recipe.name
+			  fill_in 'recipe link', :with => recipe.link
+		  end
+
+		  click_link 'Create Recipe'
 		end
-	end
 
-	it 'has a link to add a new recipe' do 
-		expect(page).to have_link('New', href: new_recipe_path(recipe))
-	end
+		it "redirects back to the index page" do
+			visit new_recipe_path
+			add_new_recipe baked_ziti
 
-	it "creates a new recipe" do
-		visit 'recipes/new'
-		within('#recipe') do
-			fill_in 'name', :with => 'example'
-			fill_in 'recipe link', :with => 'http://www.foodnetwork.com'
-		end
-		click_link 'Create Recipe'
-		expect(page).to have_content 'Recipe was successfully created.'
-	end
+		  expect(page).to have_content 'Recipe was successfully created.'
+	  end
 	end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe Recipe do
-  
-  it "should have content" do
-  	visit '/recipes'
-  	expect(page).to be_valid
-  end
 
   it "has an array of tags" do 
   	expect(recipes.tags).to eq([])
@@ -13,16 +8,16 @@ describe Recipe do
 #end
 
 #describe Tags do
-
-	it "should be connected to a user" do 
+  
+	it "connects to a user" do 
 		expect(tags).to respond_to(:link_to)
 	end
 
-	it "should be connected to a recipe" do
+	it "connects to a recipe" do
 		expect(tags).to respond_to(:recipe)
 	end
 
-	it "should sort based on the user" do 
+	it "sorts based on the user" do 
 		expect(tags).to respond_to(:user)
 	end
 end
